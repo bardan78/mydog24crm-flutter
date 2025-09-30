c3941bec4f8e09978345902a88232f2fbf760865
daniel <daniel4bar@gmail.com>
2025-09-30 13:11:16 +0200

    dalsze ustawienia koloru tła i ikony w SearchVisitTile

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           shadowColor: Colors.transparent,
         ),
       ),
-      home: const MyHomePage(title: 'Mydog CRM'),
+      home: const MyHomePage(title: 'Mydog24 CRM'),
     );
   }
 }
